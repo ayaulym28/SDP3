79d7f91b6c00c92b98de535d22e5e91f24230a5f
Ayaulym Kenzhekul <154233552+ayaulym28@users.noreply.github.com>
2024-10-09 14:16:48 +0500

    InventoryManager.java

---
+++ InventoryManager.java
@@ -0,0 +1,7 @@
+public class InventoryManager {
+    public boolean checkStock(String productId) {
+        // Логика проверки наличия товара
+        System.out.println("Checking availability of '" + productId + "' in stock...");
+        return true; // Предполагаем, что товар всегда есть в наличии
+    }
+}
