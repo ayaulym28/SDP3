f43770c7dac38b25daba571b3e9c406a9ff7eadf
Ayaulym Kenzhekul <154233552+ayaulym28@users.noreply.github.com>
2024-10-09 14:17:51 +0500

    ProductCatalog.java

---
+++ ProductCatalog.java
@@ -0,0 +1,6 @@
+public class ProductCatalog {
+    public String findProduct(String productId) {
+        // Здесь может быть логика для поиска продукта в базе данных
+        return "Product '" + productId + "' is available for purchase.";
+    }
+}
